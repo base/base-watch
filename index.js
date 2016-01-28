97872ec9c71139cc39dee6604ebb2ea23ad7a8da
Brian Woodward <brian.woodward@gmail.com>
2016-01-28 15:29:45 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * base-watch <https://github.com/base/base-watch>
+ *
+ * Copyright (c) 2016, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
